16fb6fdab8bb62b830808f67915f4bffc30de353
Nusa <arthamenova@gmail.com>
2025-04-30 17:18:20 +0700

    [update] timer

--- Assets/00_Project/Scripts/Common/DailyTimerDefinition.cs
+++ Assets/00_Project/Scripts/Common/DailyTimerDefinition.cs
@@ -11,7 +11,14 @@ namespace LegionKnight
         private int m_ResetClockHour = 15;
         public override void StartTimer(UnityAction callback = null)
         {
-            DateTime resetTime = new (DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1, m_ResetClockHour, 0, 0);
+            DateTime resetTime = new (DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, m_ResetClockHour, 0, 0);
+
+            if (DateTime.Now >= resetTime)
+            {
+                resetTime = resetTime.AddDays(1);
+            }
+
+            string resetDateString = resetTime.ToString("yyyy-MM-dd HH:mm:ss");
             Player.Instance.SetResetTime(this, resetTime);
             callback?.Invoke();
         }
