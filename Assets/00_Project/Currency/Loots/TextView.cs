3588235ae6d9312139f44a75244674d0ff260d19
Nusa <arthamenova@gmail.com>
2025-10-14 09:52:38 +0700

    Credit

--- Assets/00_Project/Currency/Loots/TextView.cs
+++ Assets/00_Project/Currency/Loots/TextView.cs
@@ -7,7 +7,7 @@ namespace LegionKnight
     {
         [SerializeField]
         private TextMeshProUGUI m_Text;
-
+        public TextMeshProUGUI Text => m_Text;
         public void SetText(string set)
         {
             m_Text.text = set;
