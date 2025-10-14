b662b060727c422d979ac168ec7acdee57892aba
Nusa <arthamenova@gmail.com>
2025-10-14 14:25:10 +0700

    Link Sosmed

--- Assets/00_Project/Credit/OpenWeb.cs
+++ Assets/00_Project/Credit/OpenWeb.cs
@@ -4,16 +4,9 @@ namespace LegionKnight
 {
     public class OpenWeb : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        public void OpenWebsite(string webLink)
         {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
+            Application.OpenURL(webLink);
         }
     }
 }
