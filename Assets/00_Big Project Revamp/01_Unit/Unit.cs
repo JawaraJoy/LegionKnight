efcb047538fd3db45c889a0f01c6d280b2908623
Nusa <arthamenova@gmail.com>
2026-01-06 15:28:18 +0700

    stat

--- Assets/00_Big Project Revamp/01_Unit/Unit.cs
+++ Assets/00_Big Project Revamp/01_Unit/Unit.cs
@@ -10,10 +10,10 @@ namespace Rush
         private UnitConfig m_Config;
 
         [SerializeField] // change tp progression monobehaviour later
-        private ProgressField m_Progresstion;
+        private ProgressField m_Progression;
         // add Stats Modifier MonoBehaviour later
         public UnitConfig Config => m_Config;
-        public ProgressField Progression => m_Progresstion;
+        public ProgressField Progression => m_Progression;
 
         [SerializeField]
         private UnityEvent<UnitContext> m_OnInit;
