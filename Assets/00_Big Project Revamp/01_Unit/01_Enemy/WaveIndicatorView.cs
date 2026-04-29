6f266c15a17fe83134f632082a3206d0fb6cc3b6
Nusa Andaria <arthamenova@gmail.com>
2026-04-29 12:55:38 +0700

    game voer

--- Assets/00_Big Project Revamp/01_Unit/01_Enemy/WaveIndicatorView.cs
+++ Assets/00_Big Project Revamp/01_Unit/01_Enemy/WaveIndicatorView.cs
@@ -8,17 +8,9 @@ namespace Rush
         [SerializeField]
         private Image m_WaveIcon;
 
-        public override void SetSlider(int current, int max)
-        {
-            base.SetSlider(current, max);
-        }
         public void SetWaveIcon(Sprite icon)
         {
             m_WaveIcon.sprite = icon;
-        }
-        override protected void ReduceDuration(float value)
-        {
-            //base.ReduceDuration(value);
         }
     }
 }
