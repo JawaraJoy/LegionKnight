89f7a4eabeea774d07d9c885add9a48f751fe069
Nusa Andaria <arthamenova@gmail.com>
2026-05-20 11:38:02 +0700

    claude mcp

--- Assets/01_Project_Old/Character/UseButton.cs
+++ Assets/01_Project_Old/Character/UseButton.cs
@@ -1,3 +1,4 @@
+using MoreMountains.Tools;
 using Rush;
 using TMPro;
 using UnityEngine;
@@ -9,10 +10,14 @@ namespace LegionKnight
     {
         [SerializeField]
         private Button m_UseButton;
+        [SerializeField]
+        private Button m_WatchToUTryOnceButton;
 
         [SerializeField]
         private TextMeshProUGUI m_UseText;
-
+        [SerializeField]
+        private TextMeshProUGUI m_WatchToTryOnceText;
+        [SerializeField, MMReadOnly]
         private HeroUnit m_HeroUnit;
 
         private void Start()
@@ -28,15 +33,27 @@ namespace LegionKnight
         }
         private void Refresh()
         {
+            bool isHeroUnlocked = m_HeroUnit.Owned;
             bool isCharacterUsed = m_HeroUnit.IsUsed;
             m_UseButton.interactable = !isCharacterUsed;
 
             m_UseText.text = isCharacterUsed ? "Used" : "Use";
+            m_WatchToUTryOnceButton.gameObject.SetActive(!isHeroUnlocked);
+            m_WatchToTryOnceText.text = !isHeroUnlocked ? "Watch to Try Once" : "";
         }
         private void Use()
         {
             Player.Instance.HeroesCollection.SetUsedHero();
             Refresh();
+        }
+        private void WatchToTryOnce()
+        {
+
+        }
+
+        private void UnlockHero()
+        {
+            m_HeroUnit.SetOwned(true);
         }
     }
 }
