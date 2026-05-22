cbd6e483dc6eb4a9a759a887fa2252a027418226
Nusa Andaria <arthamenova@gmail.com>
2026-05-22 19:07:26 +0700

    ads

--- Assets/00_Big Project Revamp/DailyAdsRewards/DailyAdsBundleView.cs
+++ Assets/00_Big Project Revamp/DailyAdsRewards/DailyAdsBundleView.cs
@@ -1,61 +1,96 @@
-using UnityEngine;
+using System;
 using LegionKnight;
 using MoreMountains.Tools;
-using UnityEngine.UI;
 using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
 namespace Rush
 {
     public class DailyAdsBundleView : UIView
     {
         [SerializeField, MMReadOnly]
         private DailyAdsBundleConfig m_Config;
+
         [SerializeField]
         private Image m_RewardIcon;
+
         [SerializeField]
         private Image m_Frame;
+
         [SerializeField]
         private TextMeshProUGUI m_RewardNameText;
+
         [SerializeField]
         private TextMeshProUGUI m_ButtonText;
+
         [SerializeField]
         private Button m_WatchButton;
+
         [SerializeField]
         private string m_WatchButtonLabel = "Watch Ad";
+
         [SerializeField]
-        private string m_GrantedButtonLabel = "Granted!";
+        private string m_GrantedButtonLabel = "Claimed";
+
+        private int m_Index;
+        private bool m_IsClaimed;
 
         public Image RewardIcon => m_RewardIcon;
         public TextMeshProUGUI RewardNameText => m_RewardNameText;
         public Button WatchButton => m_WatchButton;
-        public string WatchButtonText => m_WatchButtonLabel;
-        public string GrantedButtonText => m_GrantedButtonLabel;
 
         private ShopResultPanel m_ResultPanel;
+
         private ShopResultPanel ResultPanel
         {
             get
             {
                 if (m_ResultPanel == null)
                     m_ResultPanel = CanvasManager.Instance.GetPanel<ShopResultPanel>();
+
                 return m_ResultPanel;
             }
         }
-        public void Init(DailyAdsBundleConfig config)
+
+        public void Init(DailyAdsBundleConfig config, int index)
         {
-            InitInternal(config);
+            InitInternal(config, index);
         }
 
-        private void InitInternal(DailyAdsBundleConfig config)
+        private void InitInternal(DailyAdsBundleConfig config, int index)
         {
             m_Config = config;
-            m_RewardIcon.sprite = m_Config.Reward.CollectibleField.Icon;
-            m_Frame.color = m_Config.Reward.CollectibleField.RarityConfig.Color;
+            m_Index = index;
+
+            m_RewardIcon.sprite = m_Config.CollectibleField.Icon;
+            m_Frame.color = m_Config.CollectibleField.RarityConfig.Color;
+
             int rewardAmount = m_Config.RewardAmount;
-            m_RewardNameText.text = $"{m_Config.Reward.BaseInfo.Name} x{rewardAmount}";
+
+            m_RewardNameText.text =
+                $"{m_Config.Reward.BaseInfo.Name} x{rewardAmount}";
+
+            m_WatchButton.onClick.RemoveAllListeners();
             m_WatchButton.onClick.AddListener(WatchAds);
 
-            m_WatchButton.interactable = false;
-            m_ButtonText.text = GrantedButtonText;
+            RefreshState();
+        }
+        private void RefreshState()
+        {
+            m_IsClaimed =
+                RushPlayer.Instance.DailyAdsBundleManager.IsBundleClaimed(m_Index);
+
+            if (m_IsClaimed)
+            {
+                m_WatchButton.interactable = false;
+                m_ButtonText.text = m_GrantedButtonLabel;
+            }
+            else
+            {
+                m_WatchButton.interactable = true;
+                m_ButtonText.text = m_WatchButtonLabel;
+            }
         }
 
         private void WatchAds()
@@ -66,14 +101,23 @@ namespace Rush
         private void GrantReward()
         {
             m_Config.GrantReward();
+
+            RushPlayer.Instance.DailyAdsBundleManager.SetBundleClaimed(m_Index);
+
+            m_IsClaimed = true;
+
+            m_WatchButton.interactable = false;
+            m_ButtonText.text = m_GrantedButtonLabel;
+
             ResultPanel.Show(BuildResultData());
-            
         }
 
         private CollectibleResultData BuildResultData()
         {
             var data = new CollectibleResultData();
+
             data.AddEntry(m_Config.Reward, m_Config.RewardAmount);
+
             return data;
         }
     }
--- Assets/00_Big Project Revamp/DailyAdsRewards/DailyAdsRewardHandler.cs
+++ Assets/00_Big Project Revamp/DailyAdsRewards/DailyAdsRewardHandler.cs
@@ -1,5 +1,6 @@
 using LegionKnight;
 using MoreMountains.Tools;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -15,24 +16,31 @@ namespace Rush
 
         [SerializeField]
         private int m_MaxBundlesToSelect = 3;
+
         [SerializeField]
         private DailyAdsBundleConfig[] m_Bundles;
+
         [SerializeField, MMReadOnly]
-        private List<DailyAdsBundleConfig> m_TodaySelectedBundle = new List<DailyAdsBundleConfig>();
+        private List<DailyAdsBundleConfig> m_TodaySelectedBundle = new();
+
         private DailyAdsRewardPanel m_Panel;
 
         [SerializeField]
         private UnityEvent<DailyAdsBundleConfig[]> m_OnBundlesUpdate;
+
         public UnityEvent<DailyAdsBundleConfig[]> OnBundlesUpdate => m_OnBundlesUpdate;
+
         private DailyAdsRewardPanel Panel
         {
             get
             {
                 if (m_Panel == null)
                     m_Panel = CanvasManager.Instance.GetPanel<DailyAdsRewardPanel>();
+
                 return m_Panel;
             }
         }
+
         public void Init()
         {
             InitInternal();
@@ -40,47 +48,96 @@ namespace Rush
 
         private void InitInternal()
         {
-            if (UnityService.Instance.HasData(LastRandomize))
+            if (NeedRandomize())
             {
-                var lastRandomize = UnityService.Instance.GetData<int>(LastRandomize);
-                if (lastRandomize == System.DateTime.Now.Day)
-                {
-                    LoadSelectedBundles();
-                    return;
-                }
+                RandomizeBundles();
             }
-            RandomizeBundles();
+            else
+            {
+                LoadSelectedBundles();
+            }
+        }
+
+        private bool NeedRandomize()
+        {
+            if (!UnityService.Instance.HasData(LastRandomize))
+                return true;
+
+            var lastRandomize = UnityService.Instance.GetData<string>(LastRandomize);
+
+            return lastRandomize != DateTime.Now.Date.ToString();
         }
 
         private void LoadSelectedBundles()
         {
             m_TodaySelectedBundle.Clear();
+
             for (int i = 0; i < m_MaxBundlesToSelect; i++)
             {
                 var bundle = UnityService.Instance.GetData<DailyAdsBundleConfig>($"{m_Key}_bundle_{i}");
+
                 if (bundle != null)
                 {
                     m_TodaySelectedBundle.Add(bundle);
                 }
             }
+
             m_OnBundlesUpdate?.Invoke(m_TodaySelectedBundle.ToArray());
         }
+
         private void RandomizeBundles()
         {
             m_TodaySelectedBundle.Clear();
+
             var bundles = new List<DailyAdsBundleConfig>(m_Bundles);
+
             for (int i = 0; i < m_MaxBundlesToSelect; i++)
             {
                 if (bundles.Count == 0)
                     break;
-                int randomIndex = Random.Range(0, bundles.Count);
+
+                int randomIndex = UnityEngine.Random.Range(0, bundles.Count);
+
                 var selectedBundle = bundles[randomIndex];
+
                 m_TodaySelectedBundle.Add(selectedBundle);
+
                 UnityService.Instance.SaveData($"{m_Key}_bundle_{i}", selectedBundle);
+
+                ResetBundleClaimState(i);
+
                 bundles.RemoveAt(randomIndex);
             }
-            UnityService.Instance.SaveData(LastRandomize, System.DateTime.Now.Day);
-            
+
+            UnityService.Instance.SaveData(LastRandomize, DateTime.Now.Date.ToString());
+
+            m_OnBundlesUpdate?.Invoke(m_TodaySelectedBundle.ToArray());
+        }
+
+        private void ResetBundleClaimState(int index)
+        {
+            UnityService.Instance.SaveData(GetBundleClaimKey(index), false);
+        }
+
+        public bool IsBundleClaimed(int index)
+        {
+            return UnityService.Instance.GetData<bool>(GetBundleClaimKey(index));
+        }
+
+        public void SetBundleClaimed(int index)
+        {
+            UnityService.Instance.SaveData(GetBundleClaimKey(index), true);
+        }
+
+        private string GetBundleClaimKey(int index)
+        {
+            return $"{m_Key}_claimed_{index}";
+        }
+
+        public TimeSpan GetRemainingResetTime()
+        {
+            DateTime nextReset = DateTime.Now.Date.AddDays(1);
+            return nextReset - DateTime.Now;
         }
     }
 }
