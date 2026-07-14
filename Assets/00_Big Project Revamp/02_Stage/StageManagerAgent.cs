cf655cc1bcc8c1ca9d5bbfe9a605c1e115d997b2
Nusa Andaria <arthamenova@gmail.com>
2026-07-14 14:52:51 +0700

    Remove Aether Button

--- Assets/00_Big Project Revamp/02_Stage/StageManagerAgent.cs
+++ Assets/00_Big Project Revamp/02_Stage/StageManagerAgent.cs
@@ -10,11 +10,11 @@ namespace Rush
         }
         public void Resume()
         {
-            RushGameManager.Instance.StageManager.Resume();
+            //RushGameManager.Instance.StageManager.Resume();
         }
         public void Pause()
         {
-            RushGameManager.Instance.StageManager.Pause();
+            //RushGameManager.Instance.StageManager.Pause();
         }
         public void SelectStage(StageConfig stage)
         {
