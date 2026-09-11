4b469ee8d305f2f718ec87177a30e4c23e7fab60
Nusa Andaria <arthamenova@gmail.com>
2026-09-11 15:02:03 +0700

    iron source

--- Assets/Samples/Push Notifications/4.0.2/Push Notification Example/PushNotificationExample.cs
+++ Assets/Samples/Push Notifications/4.0.2/Push Notification Example/PushNotificationExample.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
-using Unity.Services.Analytics;
+//using Unity.Services.Analytics;
 using Unity.Services.Core;
-using Unity.Services.PushNotifications;
+//using Unity.Services.PushNotifications;
 using UnityEngine;
 
 public class PushNotificationExample : MonoBehaviour
@@ -11,16 +11,16 @@ public class PushNotificationExample : MonoBehaviour
     {
         await UnityServices.InitializeAsync();
 
-        PushNotificationsService.Instance.OnRemoteNotificationReceived += PushNotificationRecieved;
+        //PushNotificationsService.Instance.OnRemoteNotificationReceived += PushNotificationRecieved;
 
         // Note: This is the minimum required to ensure the events with the push notification data are sent correctly through Analytics.
         // In a real game you would need to handle privacy consent states here, see the Analytics documentation for more details:
         // https://docs.unity.com/ugs/en-us/manual/analytics/manual/manage-data-privacy
-        AnalyticsService.Instance.StartDataCollection();
+        //AnalyticsService.Instance.StartDataCollection();
 
         // Make sure to set the required settings in Project Settings before testing
-        string token = await PushNotificationsService.Instance.RegisterForPushNotificationsAsync();
-        Debug.Log($"The push notification token is {token}");
+        //string token = await PushNotificationsService.Instance.RegisterForPushNotificationsAsync();
+        //Debug.Log($"The push notification token is {token}");
     }
 
     void PushNotificationRecieved(Dictionary<string, object> notificationData)
